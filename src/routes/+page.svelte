9622242be17152a382c7e28c41e9fe3920ac280c
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-07-10 10:04:10 +0200

    installed shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<Button variant="outline">Button</Button>
+<Button>Button</Button>
+<Button variant="destructive">Destructive</Button>
