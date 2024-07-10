df443227b5200e93d6f6686e88901825e32645c3
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-07-10 15:22:21 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { Button } from '$lib/components/ui/button';
+	function sayHello() {
+		alert('Hello World!');
+	}
 </script>
 
-<Button variant="outline">Button</Button>
+<Button variant="outline" on:click={sayHello}>Say Hello</Button>
 <Button>Button</Button>
 <Button variant="destructive">Destructive</Button>
