14f0713c3ac5c2c5f7b2a703df3419bd16973b55
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2024-10-07 22:02:06 +0200

    added icons, Attribute-Component

---
+++ src/lib/components/Attribute.svelte
@@ -0,0 +1,15 @@
+<script>
+	import 'iconify-icon';
+
+	export let key;
+	export let value;
+	export let icon;
+</script>
+
+<div class="flex items-center gap-2">
+	<iconify-icon {icon} class="text-4xl"></iconify-icon>
+	<div class="leading-tight">
+		<p class="font-semibold">{key}</p>
+		<p>{value}</p>
+	</div>
+</div>
--- src/routes/[kunde]/+page.svelte
+++ src/routes/[kunde]/+page.svelte
@@ -1,5 +1,21 @@
 <script>
 	import { page } from '$app/stores';
+	import Attribute from '$lib/components/Attribute.svelte';
+
+	const dummyData = {
+		name: 'Maco Köck',
+		mail: 'koeck.maco@johak.at'
+	};
+
+	let name = 'Markus Seiwald';
+	let mail = 'seiwald.markus@johak.at';
 </script>
 
 <h1>Kunde {$page.params.kunde}</h1>
+
+{#each Object.entries(dummyData) as [key, value]}
+	<Attribute {key} {value}></Attribute>
+{/each}
+
+<Attribute key="Name" value={name} icon="lucide-user"></Attribute>
+<Attribute key="Mail" value={mail} icon="lucide-mail"></Attribute>
