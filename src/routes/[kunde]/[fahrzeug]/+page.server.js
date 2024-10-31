8e3be385d1d0331048ccaa058f97490042b36ca8
stefanieauer <80815387+stefanieauer@users.noreply.github.com>
2024-10-31 15:49:11 +0100

    0
    0

--- src/routes/[kunde]/[fahrzeug]/+page.server.js
+++ src/routes/[kunde]/[fahrzeug]/+page.server.js
@@ -7,13 +7,11 @@ export async function load({ params }) {
 
 	const fahrzeug = await pb.collection('Fahrzeug').getOne(params.fahrzeug);
 
-
 	const auftrag = await pb.collection('Auftrag').getList(1, 50, {
 		filter: `FahrzeugID~"${params.fahrzeug}"`
 	});
 
-    kunde.fahrzeuge = fahrzeuge;
+	kunde.fahrzeuge = fahrzeuge;
 	kunde.auftrag = auftrag;
 	return kunde;
-
 }
--- src/routes/[kunde]/[fahrzeug]/+page.svelte
+++ src/routes/[kunde]/[fahrzeug]/+page.svelte
@@ -8,9 +8,18 @@
 
 {JSON.stringify(data)}
 
-<h1>Kunde {$page.params.kunde} > Fahrzeug {$page.params.fahrzeug}</h1>
+<!-- <h1>Kunde {$page.params.kunde} > Fahrzeug {$page.params.fahrzeug}</h1> -->
+<hr />
 
-
-{#each Object.entries(data).filter((item) => item[0] === 'Kennzeichen' || item[0] === 'Marke' || item[0] === 'Modell' || item[0] === 'Erstzulassung') as [key, value]}
+{#each Object.entries(data).filter((item) => item[0] === 'Vorname' || item[0] === 'Marke' || item[0] === 'Modell' || item[0] === 'Erstzulassung') as [key, value]}
 	<Attribute {key} {value} icon={icons[key]}></Attribute>
 {/each}
+
+{#if data.auftrag.items.length > 0}
+	{#each data.auftrag.items as Auftrag (Auftrag.id)}
+		<a href="/{$page.params.kunde}/{Auftrag.id}" class="flex items-center gap-2 leading-tight">
+			<iconify-icon icon="lucide-car" class="text-4xl"></iconify-icon>
+			{Auftrag.Arbeiten}
+		</a>
+	{/each}
+{/if}
