11bd038c97b186efd9163e6190bfdf6575092b1e
stefanieauer <80815387+stefanieauer@users.noreply.github.com>
2024-11-15 16:05:34 +0100

    .

--- src/routes/[kunde]/+page.svelte
+++ src/routes/[kunde]/+page.svelte
@@ -29,51 +29,51 @@
 		class="bg-gray-100 hover:bg-gray-200 rounded-lg px-3 py-2 me-2 mb-"
 		on:click={() => (showCard = true)}>Fahrzeuganlegung</button
 	>
-
-	<!-- Fahrzeuganlegung -->
-	{#if showCard}
-		<Card.Root class="w-[700px] ">
-			<Card.Header>
-				<Card.Title>Neues Fahrzeug anlegen</Card.Title>
-			</Card.Header>
-			<Card.Content>
-				<form>
-					<div class="grid w-full items-center gap-4">
-						<div class="flex flex-col space-y-1.5">
-							<Label for="kennzeichen">Kennezeichen</Label>
-							<Input type="kennzeichen" placeholder="JO-123AB" class="max-w-xs" />
+	<div class="">
+		<!-- Fahrzeuganlegung -->
+		{#if showCard}
+			<Card.Root class="w-[700px]  ">
+				<Card.Header>
+					<Card.Title>Neues Fahrzeug anlegen</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<form>
+						<div class="grid w-full items-center gap-4">
+							<div class="flex flex-col space-y-1.5">
+								<Label for="kennzeichen">Kennezeichen</Label>
+								<Input type="kennzeichen" placeholder="JO-123AB" class="max-w-xs" />
+							</div>
+							<div class="flex flex-col space-y-1.5">
+								<Label for="marke">Marke</Label>
+								<Input type="marke" placeholder="VW" class="max-w-xs" />
+							</div>
+							<div class="flex flex-col space-y-1.5">
+								<Label for="modell">Modell</Label>
+								<Input type="modell" placeholder="Golf 7" class="max-w-xs" />
+							</div>
+							<div class="flex flex-col space-y-1.5">
+								<Label for="erstzulassung">Erstzulassung</Label>
+								<Input type="erstzulassung" placeholder="2015-09-05" class="max-w-xs" />
+							</div>
+							<div class="flex flex-col space-y-1.5">
+								<Label for="zulassungschein">Zulassungschein</Label>
+								<Input type="zulassungschein" placeholder="Foto" class="max-w-xs" />
+							</div>
 						</div>
-						<div class="flex flex-col space-y-1.5">
-							<Label for="marke">Marke</Label>
-							<Input type="marke" placeholder="VW" class="max-w-xs" />
-						</div>
-						<div class="flex flex-col space-y-1.5">
-							<Label for="modell">Modell</Label>
-							<Input type="modell" placeholder="Golf 7" class="max-w-xs" />
-						</div>
-						<div class="flex flex-col space-y-1.5">
-							<Label for="erstzulassung">Erstzulassung</Label>
-							<Input type="erstzulassung" placeholder="2015-09-05" class="max-w-xs" />
-						</div>
-						<div class="flex flex-col space-y-1.5">
-							<Label for="zulassungschein">Zulassungschein</Label>
-							<Input type="zulassungschein" placeholder="Foto" class="max-w-xs" />
-						</div>
-					</div>
-				</form></Card.Content
-			>
-
-			<Card.Footer class="flex justify-between">
-				<button class="text-black bg-gray-300 hover:bg-gray-400 rounded-lg px-3 py-2 me-2 mb-2">
-					Abrechen</button
+					</form></Card.Content
 				>
-				<button class="text-white bg-gray-800 hover:bg-gray-900 rounded-lg px-3 py-2 me-2 mb-2"
-					>Speichern</button
-				>
-			</Card.Footer>
-		</Card.Root>
-	{/if}
 
+				<Card.Footer class="flex justify-between">
+					<button class="text-black bg-gray-300 hover:bg-gray-400 rounded-lg px-3 py-2 me-2 mb-2">
+						Abrechen</button
+					>
+					<button class="text-white bg-gray-800 hover:bg-gray-900 rounded-lg px-3 py-2 me-2 mb-2"
+						>Speichern</button
+					>
+				</Card.Footer>
+			</Card.Root>
+		{/if}
+	</div>
 	{#if data.fahrzeuge.items.length > 0}
 		{#each data.fahrzeuge.items as fahrzeug (fahrzeug.id)}
 			<a href="/{$page.params.kunde}/{fahrzeug.id}" class="flex items-center gap-2 leading-tight">
