b19c2592e777753bb3991a4cbafc54565a3e90f8
stefanieauer <80815387+stefanieauer@users.noreply.github.com>
2024-12-10 21:29:27 +0100

    kleine design veränderungen
    mdjd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
 	export let data;
 	import { pb } from '$lib/pocketbase.js';
-	import * as Card from '$lib/components/ui/card';
-	import { Input } from '$lib/components/ui/input';
-	import { Label } from '$lib/components/ui/label';
 
 	// Zustand für die Sichbarkeit der Card definieren
 	let showCard = false;
@@ -43,40 +40,12 @@
 		// Alle Felder durchsuchen (Vorname, Nachname, Firma, E-Mail, etc.)
 		const fullName = `${kunde.Vorname} ${kunde.Nachname}`.toLowerCase();
 		const fullDetails =
-			`${fullName} ${kunde.Firma} ${kunde.Email} ${kunde.Telefonnummer} ${kunde.Strasse} ${kunde.Plz} ${kunde.Ort}`.toLowerCase();
+			`${fullName} ${kunde.Firma} ${kunde.Email} ${kunde.Telefonnummer} ${kunde.Strasse} ${kunde.Plz} ${kunde.Ort} ${kunde.fahrzeuge.Kennzeichen} ${kunde.fahrzeuge.FIN} ${kunde.fahrzeuge.Nat_Code}	${kunde.fahrzeuge.Marke}	${kunde.fahrzeuge.Modell} ${kunde.fahrzeuge.Erstzulassung} ${kunde.fahrzeuge.KmStand} 	${kunde.fahrzeuge.Hubraum} ${kunde.fahrzeuge.PS} 	${kunde.fahrzeuge.KW} ${kunde.fahrzeuge.Pickerl} 	${kunde.fahrzeuge.Farbcode} 	${kunde.fahrzeuge.Motorcode} ${kunde.fahrzeuge.Kraftstott} `.toLowerCase();
 		console.log(JSON.stringify(kunde));
+
 		// Prüfen, ob der Suchbegriff irgendwo im Text enthalten ist
 		return fullDetails.includes(searchLowerCase);
 	});
-
-	// async function createKunde() {
-	// 	const kundenDaten = {
-	// 		action: 'createKunde',
-	// 		kundennr,
-	// 		firma,
-	// 		vorname,
-	// 		nachname,
-	// 		email,
-	// 		telefonnummer,
-	// 		strasse,
-	// 		plz,
-	// 		ort
-	// 	};
-
-	// 	try {
-	// 		const response = await fetch('/create-client', {
-	// 			method: 'POST',
-	// 			headers: {
-	// 				'Content-Type': 'application/json'
-	// 			},
-	// 			body: JSON.stringify(kundenDaten)
-	// 		});
-	// 		const result = await response.json();
-	// 	} catch (error) {
-	// 		console.error(error);
-	// 	}
-	// 	console.log(kundenDaten);
-	// }
 
 	async function deleteKunde(kundeId) {
 		if (confirm('Möchtest du das Fahrzeug wirklich löschen?')) {
@@ -96,8 +65,6 @@
 </script>
 
 <main>
-	<!-- Mobile und Desktop Design -->
-
 	<!-- Suchfeld -->
 	<div class="pl-4 mb-6 w-full">
 		<input
@@ -113,56 +80,26 @@
 
 	<!-- Kundenliste -->
 	{#each filteredData as kunde (kunde.id)}
-		<a href="/{kunde.id}" class="flex items-center gap-4 pl-4">Kunde Details</a>
 		<a
 			href="/{kunde.id}"
 			class="flex items-center gap-4 pl-4"
 			on:click={() => updateLastOpened(kunde.id)}
 		>
 			<iconify-icon icon="lucide-user" class="text-4xl"></iconify-icon>
-			<span
-				class={searchQuery &&
-					`${
-						// Highlight Kunden, die mit dem Suchbegriff übereinstimmen
-						`${kunde.Vorname} ${kunde.Nachname}`
-							.toLowerCase()
-							.includes(searchQuery.toLowerCase()) ||
-						`${kunde.Firma}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.Email}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.Telefonnummer}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.Strasse}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.Plz}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.Ort}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Kennzeichen}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.FIN}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Nat_Code}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Marke}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Modell}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Erstzulassung}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.KmStand}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Hubraum}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.PS}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.KW}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Pickerl}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Farbcode}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Motorcode}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.fahrzeug.Kraftstott}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.auftrag.Autragnummer}`.toLowerCase().includes(searchQuery.toLowerCase()) ||
-						`${kunde.auftrag.Arbeiten}`.toLowerCase().includes(searchQuery.toLowerCase())
-							? 'bg-gray-200'
-							: ''
-					}`}
-			>
+			<span class="flex items-center gap-4">
 				{kunde.Nachname}
 				{kunde.Vorname}
-				{kunde.id}
-				<button on:click|stopPropagation|preventDefault={() => deleteKunde(kunde.id)}
-					>Löschen</button
+
+				<button
+					class="absolute right-0 px-2 py-1 bg-red-500 hover:bg-red-600 text-white rounded-lg"
+					on:click|stopPropagation|preventDefault={() => deleteKunde(kunde.id)}
 				>
+					Löschen
+				</button>
 			</span>
 		</a>
 	{/each}
 	<hr />
 </main>
 
-{JSON.stringify(filteredData)}
+<!-- {JSON.stringify(filteredData)} -->
